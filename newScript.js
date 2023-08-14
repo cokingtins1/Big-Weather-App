cc0c21268c217fdc8073813fb12678b6bd599009
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-08-14 16:19:09 -0400

    reformatted data processor

--- newScript.js
+++ newScript.js
@@ -4,6 +4,12 @@ const apiKey =
 const lat = 39.3321
 const lon = -84.4173
 
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
+
 function setLocaiton(lat, lon) {
 	window.LINK = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York`
 }
@@ -15,69 +21,119 @@ async function getWeather() {
 		const response = await fetch(LINK)
 		const data = await response.json()
 
-		//deconstruct data from API and store as const for easier access
-		const parsedData = parseData(data)
+		const weatherData = parseWeatherData(data) // get more readable/usable data from raw data
 
-		// rename parsedData to current and parseData fucntion to parseCurrentWeather. create separate const for daily weather and separate function to parsedaily data.
+		const { current, daily, hourly } = weatherData // deconstruct parsed data into current, daily, and hourly
+
+		dayMapFunction(daily)
 		console.log(data)
-		dayMapfun(parsedData)
-		renderWeather(parsedData)
+
+		renderWeather(weatherData)
 	} catch (error) {
-		console.error("there was an error")
+		console.error("There was an error:", error)
 	}
 }
 
-function parseData({ current_weather, daily, daily_units, hourly }) {
+function parseWeatherData(data) {
+	const { current_weather, daily, hourly } = data
+
 	const {
 		temperature: currentTemp,
-		windspeed: windspeed,
 		weathercode: iconCode,
-		time: time,
-	} = current_weather
-	const { apparent_temperature_max: maxDailyTemp } = daily
-	const { apparent_temperature_max: degF } = daily_units
-	const { temperature_2m: hourlyTemp } = hourly
-	return {
-		currentTemp: Math.round(currentTemp),
-		highTemp: Math.round(Math.max(...hourlyTemp)),
-		lowTemp: Math.round(Math.min(...hourlyTemp)),
-		maxDailyTemp: maxDailyTemp.map((number) => Math.round(number)),
+		windspeed: windspeed,
+		is_day: dayCode,
 		time,
-		iconCode,
-		degF,
+	} = current_weather
+
+	const {
+		temperature_2m_max: maxTemp,
+		temperature_2m_min: minTemp,
+		apparent_temperature_max: maxFeelsLike,
+		apparent_temperature_min: minFeelsLike,
+	} = daily
+
+	return {
+		current: {
+			currentTemp: Math.round(currentTemp),
+			time: time * 1000,
+			windspeed,
+			dayCode,
+			iconCode,
+			maxFeelsLike: maxFeelsLike.map((number) => Math.round(number)),
+			minFeelsLike: minFeelsLike.map((number) => Math.round(number)),
+		},
+		daily: {
+			highTemp: Math.round(Math.max(...maxTemp)),
+			lowTemp: Math.round(Math.min(...minTemp)),
+			highFeelsLike: Math.round(maxFeelsLike),
+			lowFeelsLike: Math.round(minFeelsLike),
+		},
+		hourly: hourly,
 	}
 }
 
-function renderWeather(data) {
-	renderWeeklyWeather(data)
-	renderHourlyWeather(data)
-	renderCurrentWeather(data)
+function dayMapFunction(dailyData) {
+	// Implement your logic here to process daily weather data
+}
+
+function renderWeather({ current, daily, renderHourlyWeather }) {
+	// renderWeeklyWeather(data)
+	// renderHourlyWeather(data)
+	renderCurrentWeather(current)
+	renderDailyWeather(daily)
+
 	// render weekly data, hourly data, etc.
 }
 
-function renderWeeklyWeather() {
-	for (let i = 0; i < 7; i++) {
-		setValue(`${i}`, dayMap.get(i), "F")
-	}
+function renderCurrentWeather(current) {
+	const currentDay = current.dayCode
+	console.log(currentDay)
+	// console.log(data.current_weather.temperature)
+	setValue("current-temp", current.currentTemp, "")
+	setValue("current-fl-high", current.maxFeelsLike[currentDay], "")
+	setValue("current-fl-low", current.minFeelsLike[currentDay], "")
+
+	setValue("current-wind", current.windspeed, "")
+	// setValue("current-low", current.lowFeelsLike, "°F")
 }
 
-function renderHourlyWeather(data) {
-	// const currentTime = data.current_weather.time
-	function convertTime(unixTime) {
-		const date = new Date(unixTime * 1000)
-		const hour = date.getUTCHours()
-		const minutes = date.getUTCMinutes()
+function renderDailyWeather(daily) {
+	setValue("current-high", daily.highTemp, "")
+	setValue("current-low", daily.lowTemp, "")
+	setValue("current-high", daily.highTemp, "")
+}
 
-		const period = hour >= 12 ? "PM" : "AM"
-		const ampmHour = hour % 12 === 0 ? 12 : hour % 12
-		console.log(hour)
-		// return `${ampmHour}${period}`
-		return `${ampmHour}:${minutes.toString().padStart(2, "0")} ${period}`
+function renderWeeklyWeather(data) {
+	dailySection.innerHTML = ""
+
+	data.maxDailyTemp.forEach((day) => {
+		const element = dayCardTemplate.content.cloneNode(true)
+		setValue("temp", day, "°F", { parent: element })
+		setValue("date", DAY_FORMATTER.format(day.time), "", {
+			parent: element,
+		})
+		dailySection.append(element)
+	})
+}
+
+function renderHourlyWeather() {
+	// const currentUnixTime = Math.floor(Date.now() / 1000)
+	// function convertTime(unixTime) {
+	// 	const date = new Date(unixTime * 1000)
+	// 	return new Intl.DateTimeFormat("en-US", { hour: "numeric" }).format(
+	// 		date
+	// 	)
+	// }
+
+	// const currentHour = convertTime(currentUnixTime)
+	// // console.log(currentHour[0])
+
+	let timeArray = []
+	for (let i = 0; i < 9; i++) {
+		timeArray.push(parseInt(currentHour[0]) + i)
 	}
 
-	const currentUnixTime = Math.floor(Date.now())
-	const theHour = convertTime(currentUnixTime)
-	console.log(theHour)
+	// console.log(timeArray)
 }
 
 const dayMap = new Map()
@@ -85,13 +141,6 @@ function dayMapfun(data) {
 	for (let i = 0; i < 7; i++) {
 		dayMap.set(i, data.maxDailyTemp[i])
 	}
-}
-
-function renderCurrentWeather(data) {
-	// console.log(data.current_weather.temperature)
-	setValue("current-temp", data.currentTemp, data.degF)
-	setValue("current-high", data.highTemp, data.degF)
-	setValue("current-low", data.lowTemp, data.degF)
 }
 
 function setValue(selector, value, unit, { parent = document } = {}) {
